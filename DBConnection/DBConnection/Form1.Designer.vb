f9a2249818062e9b9164449be5d51d74e570af09
unknown <kelvindisi@yahoo.com>
2020-08-04 09:35:06 +0300

    connection to mysql database

---
+++ DBConnection/DBConnection/Form1.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.lblRegNo = New System.Windows.Forms.Label()
+        Me.txtRegNo = New System.Windows.Forms.TextBox()
+        Me.lblCourse = New System.Windows.Forms.Label()
+        Me.cboCourse = New System.Windows.Forms.ComboBox()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnCancel = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Font = New System.Drawing.Font("Montserrat", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(29, 37)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(60, 22)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "Name"
+        '
+        'txtName
+        '
+        Me.txtName.Font = New System.Drawing.Font("Montserrat", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtName.Location = New System.Drawing.Point(115, 32)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(247, 27)
+        Me.txtName.TabIndex = 1
+        '
+        'lblRegNo
+        '
+        Me.lblRegNo.AutoSize = True
+        Me.lblRegNo.Font = New System.Drawing.Font("Montserrat", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRegNo.Location = New System.Drawing.Point(31, 102)
+        Me.lblRegNo.Name = "lblRegNo"
+        Me.lblRegNo.Size = New System.Drawing.Size(71, 22)
+        Me.lblRegNo.TabIndex = 2
+        Me.lblRegNo.Text = "Reg No"
+        '
+        'txtRegNo
+        '
+        Me.txtRegNo.Font = New System.Drawing.Font("Montserrat", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtRegNo.Location = New System.Drawing.Point(115, 97)
+        Me.txtRegNo.Name = "txtRegNo"
+        Me.txtRegNo.Size = New System.Drawing.Size(247, 27)
+        Me.txtRegNo.TabIndex = 1
+        '
+        'lblCourse
+        '
+        Me.lblCourse.AutoSize = True
+        Me.lblCourse.Font = New System.Drawing.Font("Montserrat", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCourse.Location = New System.Drawing.Point(31, 167)
+        Me.lblCourse.Name = "lblCourse"
+        Me.lblCourse.Size = New System.Drawing.Size(69, 22)
+        Me.lblCourse.TabIndex = 2
+        Me.lblCourse.Text = "Course"
+        '
+        'cboCourse
+        '
+        Me.cboCourse.Font = New System.Drawing.Font("Montserrat", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboCourse.FormattingEnabled = True
+        Me.cboCourse.Location = New System.Drawing.Point(115, 160)
+        Me.cboCourse.Name = "cboCourse"
+        Me.cboCourse.Size = New System.Drawing.Size(247, 30)
+        Me.cboCourse.TabIndex = 3
+        '
+        'btnSave
+        '
+        Me.btnSave.Font = New System.Drawing.Font("Montserrat", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSave.Location = New System.Drawing.Point(115, 226)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(96, 38)
+        Me.btnSave.TabIndex = 4
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnCancel
+        '
+        Me.btnCancel.Font = New System.Drawing.Font("Montserrat", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCancel.Location = New System.Drawing.Point(217, 226)
+        Me.btnCancel.Name = "btnCancel"
+        Me.btnCancel.Size = New System.Drawing.Size(96, 38)
+        Me.btnCancel.TabIndex = 4
+        Me.btnCancel.Text = "Cancel"
+        Me.btnCancel.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(400, 293)
+        Me.Controls.Add(Me.btnCancel)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.cboCourse)
+        Me.Controls.Add(Me.lblCourse)
+        Me.Controls.Add(Me.lblRegNo)
+        Me.Controls.Add(Me.txtRegNo)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lblName)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblName As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents lblRegNo As Label
+    Friend WithEvents txtRegNo As TextBox
+    Friend WithEvents lblCourse As Label
+    Friend WithEvents cboCourse As ComboBox
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnCancel As Button
+End Class
---
+++ DBConnection/DBConnection/Form1.vb
@@ -0,0 +1,43 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Public conn As MySqlConnection = New MySqlConnection()
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        Me.StartPosition = FormStartPosition.CenterScreen
+
+        cboCourse.Items.AddRange(New String() {"BscIT", "BBC", "DIT", "CIT"})
+        cboCourse.SelectedIndex = 0
+        connectToDb()
+    End Sub
+    Public Sub connectToDb()
+        conn.ConnectionString = "server=localhost;user id=root;password=;database=BIT2118"
+        Try
+            conn.Open()
+            MsgBox("Connection successful")
+        Catch ex As Exception
+            MsgBox("Connection not successfull" & vbNewLine & ex.Message)
+        End Try
+        conn.Close()
+    End Sub
+
+    Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
+        txtName.Clear()
+        txtRegNo.Clear()
+        cboCourse.SelectedIndex = 0
+    End Sub
+
+    Private Sub closeConnection()
+        Try
+            If conn.State = ConnectionState.Open Then
+                conn.Close()
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
